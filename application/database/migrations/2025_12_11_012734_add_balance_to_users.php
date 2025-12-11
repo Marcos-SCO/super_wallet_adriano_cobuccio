23918c281bf3d09b66283ce5ce79140ea7d92c3e
marcos-sco <marcos_sco@outlook.com>
2025-12-10 22:46:54 -0300

    feat: update on users table to receive balance

---
+++ application/database/migrations/2025_12_11_012734_add_balance_to_users.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // extending users table to add balance column
+            $table->decimal('balance', 14, 2)->default(0.00)->after('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // destroy the fun
+            $table->dropColumn('balance');
+        });
+    }
+};
